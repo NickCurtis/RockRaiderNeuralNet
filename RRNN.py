2df30e740b78769c2860b9692874ccb21433e09c
NickSquared <curtin@rpi.edu>
2017-04-08 01:00:56 -0400

    Added input and output

--- RRNN.py
+++ RRNN.py
@@ -2,12 +2,15 @@ import numpy as np
 
 class NeuralNet(object):
 	"""A simple image recognition neural net"""
-	def __init__(self):
-		self.layers = [None] * 9
-		np.random.seed(0)
-		for i in range(len(self.layers)):
-			self.layers[i] = np.random.random()
-			print self.layers[i]
+	def __init__(self, l = None):
+		if (l == None):
+			self.layers = [None] * 9
+			np.random.seed(0)
+			for i in range(len(self.layers)):
+				self.layers[i] = np.random.random()
+				print self.layers[i]
+		else:
+			self.layers = l
 
 	def think(self,inputs):
 		# Initialize an array of hidden layer totals to be sigmoided
@@ -36,6 +39,26 @@ class NeuralNet(object):
 def sigmoid(x):
 	return 1.0 / (1 + np.exp(-x))
 
+#return an array of floats from given file
+def read(file):
+	#strip and cast as float for each line in file
+	return [float(line.rstrip('\n')) for line in open(file)]
+
+
+def write(file,values,length,init = False):
+	f = open(file,'w')
+	#If we wish to initialize with default values
+	if init == True:
+		np.random.seed(0)
+		for i in range(length):
+			f.write('%f\n'%(np.random.random()))
+
+	#else write current values to file
+	else:
+		for i in range(len(values)):
+			f.write('%f\n',values[i])
+	f.close()
+
 
 
 
@@ -43,3 +66,5 @@ if __name__ == '__main__':
 	nn = NeuralNet()
 	inputs = [1,1]
 	nn.think(inputs)
+	#write('layers.txt',[],9,True)
+	print read('layers.txt')
