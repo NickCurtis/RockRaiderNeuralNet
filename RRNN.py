18ac69ccc213956ee15d8d70c72400033e7cf49f
NickSquared <curtin@rpi.edu>
2017-04-08 00:27:40 -0400

    Finished entry level neural network, added documentation, and cleaned up code

--- RRNN.py
+++ RRNN.py
@@ -1,10 +1,9 @@
 import numpy as np
-import sklearn
 
 class NeuralNet(object):
-	"""docstring for NeuralNet"""
+	"""A simple image recognition neural net"""
 	def __init__(self):
-		self.layers = [None] * 8
+		self.layers = [None] * 9
 		np.random.seed(0)
 		for i in range(len(self.layers)):
 			self.layers[i] = np.random.random()
@@ -12,19 +11,23 @@ class NeuralNet(object):
 
 	def think(self,inputs):
 		# Initialize an array of hidden layer totals to be sigmoided
-		#toHiddens = [None] * 6
 		hiddenTotal = [0.0] * 3
+		#initialize return total
+		total = 0.0
+		#j = 6
+
+		#Multiply all input values by 
 		for i in range(6):
 			hiddenTotal[i/2] += self.layers[i] * inputs[i%2]
-			#print "i:",inputs[i/3]
+
+		#sigmoid hidden layer and get total output layer ready for sigmoid
 		for i in range(3):
 			hiddenTotal[i] = sigmoid(hiddenTotal[i])
-			print "HIDDEN TOTAL:",hiddenTotal[i]
-			'''
-		hiddenTotal[0] = sigmoid(hiddenTotal[0])
-		hiddenTotal[1] = sigmoid(hiddenTotal[1])
-		print hiddenTotal[0],hiddenTotal[1]
-		'''
+			total += hiddenTotal[i] * self.layers[i+6] #Plus 6 here to align idexing
+
+		total = sigmoid(total)
+		print "Total:",total
+		return total
 
 
 
