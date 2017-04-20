2a9b29ef280dcd4bf32a6d17c5772a4e9d673f8c
NickSquared <curtin@rpi.edu>
2017-04-20 00:05:20 -0400

    Added a bunch of tennis ball images. Also added multiple file io for reading all images.

--- RRNN.py
+++ RRNN.py
@@ -26,6 +26,9 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 import sys
 import os
 import time
+import gzip
+import glob
+import Image
 
 import numpy as np
 import theano
@@ -39,9 +42,16 @@ import lasagne
 #========================================================================================
 
 # Load our tennis ball images
-def load():
-	pass
+def load(filename):
 
+	filelist = glob.glob('tennis_images/*.jpg')
+	data = np.array([np.array(Image.open(fname)) for fname in filelist])
+	values = [1] * len(filelist)
+
+
+
+	#print type(data)
+	return data,values
 
 # Set up the convolutional nn
 def buildNetwork(inputVal = None):
@@ -92,7 +102,53 @@ def sigDeriv(x):
 
 if __name__ == '__main__':
 
-	nn = buildNetwork()
+	#Load images
+	data,values = load('TennisBalls.tar.gz')
+
+
+	# Prepare Theano variables for inputs and targets
+	input_var = T.tensor4('inputs')
+	target_var = T.ivector('targets')
+
+	#Build the neural network using the lasagne library
+	nn = buildNetwork(input_var)
+
+
+	# Create a loss expression for training, i.e., a scalar objective we want
+	# to minimize (for our multi-class problem, it is the cross-entropy loss):
+	prediction = lasagne.layers.get_output(nn)
+	loss = lasagne.objectives.categorical_crossentropy(prediction, target_var)
+	loss = loss.mean()
+	# We could add some weight decay as well here, see lasagne.regularization.
+
+	# Create update expressions for training, i.e., how to modify the
+	# parameters at each training step. Here, we'll use Stochastic Gradient
+	# Descent (SGD) with Nesterov momentum, but Lasagne offers plenty more.
+	params = lasagne.layers.get_all_params(nn, trainable=True)
+	updates = lasagne.updates.nesterov_momentum(
+	        loss, params, learning_rate=0.01, momentum=0.9)
+
+	# Create a loss expression for validation/testing. The crucial difference
+	# here is that we do a deterministic forward pass through the network,
+	# disabling dropout layers.
+	test_prediction = lasagne.layers.get_output(nn, deterministic=True)
+	test_loss = lasagne.objectives.categorical_crossentropy(test_prediction,
+	                                                        target_var)
+	test_loss = test_loss.mean()
+	# As a bonus, also create an expression for the classification accuracy:
+	test_acc = T.mean(T.eq(T.argmax(test_prediction, axis=1), target_var),
+	                  dtype=theano.config.floatX)
+
+	# Compile a function performing a training step on a mini-batch (by giving
+	# the updates dictionary) and returning the corresponding training loss:
+	train_fn = theano.function([input_var, target_var], loss, updates=updates)
+
+	# Compile a second function computing the validation loss and accuracy:
+	val_fn = theano.function([input_var, target_var], [test_loss, test_acc])
+
+	for _ in range(1):
+		theano.function([input_var, target_var], loss, updates=updates)
+	
 
 	np.savez('layers.txt', *lasagne.layers.get_all_param_values(nn))
 
