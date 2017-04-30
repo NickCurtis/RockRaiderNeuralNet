51002fd00490b233150076d77203bcf23112eb10
NickSquared <curtin@rpi.edu>
2017-04-29 21:17:53 -0400

    Reorganized file directory and catkin_made the vision_neural_net to act as this projects package

---
+++ vision_neural_net/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/nick/catkin_ws/src/RockRaiderNeuralNet/vision_neural_net
+# Build directory: /home/nick/catkin_ws/src/RockRaiderNeuralNet/vision_neural_net
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
